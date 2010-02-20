9d74e2556570db850d7497edf648e6d953d41ad8
Matthew Fredrickson <matt@short.local>
2010-02-20 17:45:57 +0100

    Make sure that libsccp builds on my intel Mac ok. Apparently endian.h is in a special directory

--- openbsc/include/sccp/sccp_types.h
+++ openbsc/include/sccp/sccp_types.h
@@ -24,7 +24,11 @@
 #ifndef SCCP_TYPES_H
 #define SCCP_TYPES_H
 
+#ifdef __APPLE__
+#include <i386/endian.h>
+#else
 #include <endian.h>
+#endif
 
 /* Table 1/Q.713 - SCCP message types */
 enum sccp_message_types {
